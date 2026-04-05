8cacda6fd883d52fa8d0a61422f27f53e7b55a3a
niri2630 <niriksh.shetty2630@gmail.com>
2026-04-05 17:21:38 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "claybag.com",
+      },
+      {
+        protocol: "https",
+        hostname: "claybag-media-prod.s3.ap-south-1.amazonaws.com",
+      },
       {
         protocol: "http",
         hostname: "localhost",
         port: "8000",
-        pathname: "/uploads/**",
       },
     ],
   },
